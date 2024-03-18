8b8b6630aa9f1422928af535a7f576eee7cde2a3
CGX <cgx871220@gmail.com>
2024-03-19 00:12:11 +0800

    add project data

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
-# 這是我的個人履歷網頁
+# 我的個人履歷網頁
  我是陳冠憲，於2021年畢業於高雄正修科技大學資訊工程系， 透過大學課業，我接觸到HTML與CSS，引發我對前端開發的熱情。在空閒時間，我積極透過網路學習Javascript、Bootstrap和Vue等前端相關技術。
 
-![TitlePage](/src/assets/ProjectPic/titlepage.png)
+## 使用技術
+* HTML/CSS
+* javascript
+* scss
+* Bootstrap
+* vue
+
+## 頁面連結
+[https://cgx871220.github.io/resume/dist/](<https://cgx871220.github.io/resume/dist/>)
+
+
+## 網頁截圖
+![TitlePage](/src/assets/titlepage.png)
--- src/components/ProjectComponent.vue
+++ src/components/ProjectComponent.vue
@@ -3,15 +3,15 @@
 <h2 class="mb-5">我的作品</h2>
   <div class="row row-cols-md-2 row-cols-lg-3 gy-2 gy-md-3 gx-3">
     <div   v-for="Project in ProjectList" :key="Project.title">
-      <div>
-        <div class="card text-center" >
-          <img :src="Project.src" class="card-img-top" :alt="Project.title" />
-          <div class="card-body">
-            <h5 class="card-title">{{ Project.title }}</h5>
+      <div class="h-100">
+        <div class="card text-center h-100" >
+          <img :src="Project.src" class="card-img-top" :alt="Project.title" style="height: 40%;"/>
+          <div class="card-body d-flex flex-column">
+            <h5 class="card-title mt-auto font-bold fw-semibold">{{ Project.title }}</h5>
             <p class="card-text">
               {{ Project.content }}
             </p>
-            <a :href="Project.github" target="_blank" class="btn btn-dark">Github</a>
+            <a :href="Project.github" target="_blank" class="btn btn-dark mt-auto">Github</a>
           </div>
         </div>
       </div>
@@ -23,40 +23,19 @@
 const ProjectList = [
   {
     title: '高雄景點資訊',
-    src: import.meta.env.VITE_PROJECTURL,
-    skill: '利用 Vite 建立專案、使用 AJAX 撈取 API 資料、Bootstrap 格線系統排版',
-    content:
-      '在這個專案中，我運用了 Vite 作為建立前端專案的工具，同時透過 AJAX 技術從外部 API 撈取資料,並使用了 Bootstrap 的格線系統來實現簡單且響應式的網頁排版',
-    github:'https://github.com/cgx871220/KaohSiung-Travel'  
-  },{
-    title: '高雄景點資訊',
-    src: import.meta.env.VITE_PROJECTURL,
-    skill: '利用 Vite 建立專案、使用 AJAX 撈取 API 資料、Bootstrap 格線系統排版',
-    content:
-      '在這個專案中，我運用了 Vite 作為建立前端專案的工具，同時透過 AJAX 技術從外部 API 撈取資料,並使用了 Bootstrap 的格線系統來實現簡單且響應式的網頁排版',
-    github:'https://github.com/cgx871220/KaohSiung-Travel'  
-  },{
-    title: '高雄景點資訊',
-    src: import.meta.env.VITE_PROJECTURL,
-    skill: '利用 Vite 建立專案、使用 AJAX 撈取 API 資料、Bootstrap 格線系統排版',
-    content:
-      '在這個專案中，我運用了 Vite 作為建立前端專案的工具，同時透過 AJAX 技術從外部 API 撈取資料,並使用了 Bootstrap 的格線系統來實現簡單且響應式的網頁排版',
-    github:'https://github.com/cgx871220/KaohSiung-Travel'  
-  },{
-    title: '高雄景點資訊',
-    src: import.meta.env.VITE_PROJECTURL,
-    skill: '利用 Vite 建立專案、使用 AJAX 撈取 API 資料、Bootstrap 格線系統排版',
-    content:
-      '在這個專案中，我運用了 Vite 作為建立前端專案的工具，同時透過 AJAX 技術從外部 API 撈取資料,並使用了 Bootstrap 的格線系統來實現簡單且響應式的網頁排版',
-    github:'https://github.com/cgx871220/KaohSiung-Travel'  
-  },{
-    title: '高雄景點資訊',
-    src: import.meta.env.VITE_PROJECTURL,
+    src: import.meta.env.VITE_KaohSiungTravel,
     skill: '利用 Vite 建立專案、使用 AJAX 撈取 API 資料、Bootstrap 格線系統排版',
     content:
       '在這個專案中，我運用了 Vite 作為建立前端專案的工具，同時透過 AJAX 技術從外部 API 撈取資料,並使用了 Bootstrap 的格線系統來實現簡單且響應式的網頁排版',
     github:'https://github.com/cgx871220/KaohSiung-Travel'  
   },
+  {
+    title:'todolist',
+    src: import.meta.env.VITE_todolist,
+    skill:'利用Vite建立專案,使用Vue基礎語法及Composition API撰寫、使用Bootstrap製作響應式頁面',
+    content:'將基本的Vue optionAPI更改為較進階的Composition API,並加上了localsorage將資料儲存在瀏覽器',
+    github:'https://github.com/cgx871220/todolist'
+  }
 ]
 </script>
 
